f92066b45811e4aecb519389616808c91eb34c6f
Ftmoil <96582224+Ftmoil@users.noreply.github.com>
2021-12-23 18:27:41 +0100

    Update config.php

--- application/config.php
+++ application/config.php
@@ -3,5 +3,7 @@ $db_host	    = 'localhost';
 $db_user        = 'root';
 $db_password	= 'Kapala1992';
 $db_name	    = 'invoice';
-define('APP_URL', 'http://192.168.0.4/ftm');
+define('APP_URL', 'http://Ftmoil
+/
+Ftmcaissefc.com/');
 $_app_stage = 'Live'; // 
